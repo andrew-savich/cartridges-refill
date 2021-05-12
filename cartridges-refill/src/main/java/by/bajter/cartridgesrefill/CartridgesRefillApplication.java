6884155fdc0f699593cecfd03c573ea2133ef6be
andrew-savich <savichandrew@Gmail.com>
2021-05-12 22:54:22 +0300

    Init commit

---
+++ cartridges-refill/src/main/java/by/bajter/cartridgesrefill/CartridgesRefillApplication.java
@@ -0,0 +1,13 @@
+package by.bajter.cartridgesrefill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartridgesRefillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartridgesRefillApplication.class, args);
+	}
+
+}
---
+++ cartridges-refill/src/test/java/by/bajter/cartridgesrefill/CartridgesRefillApplicationTests.java
@@ -0,0 +1,13 @@
+package by.bajter.cartridgesrefill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartridgesRefillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
