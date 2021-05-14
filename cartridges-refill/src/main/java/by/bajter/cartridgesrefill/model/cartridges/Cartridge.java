a900608484d08fb674248a2514678f090522e868
andrew-savich <savichandrew@Gmail.com>
2021-05-14 18:59:09 +0300

    Improved model

--- cartridges-refill/src/main/java/by/bajter/cartridgesrefill/model/cartridges/Cartridge.java
+++ cartridges-refill/src/main/java/by/bajter/cartridgesrefill/model/cartridges/Cartridge.java
@@ -1,13 +1,10 @@
 package by.bajter.cartridgesrefill.model.cartridges;
 
-import java.util.List;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import by.bajter.cartridgesrefill.model.Refill;
 import by.bajter.cartridgesrefill.model.client.Client;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,9 +18,7 @@ public class Cartridge {
 	private Long id;
 	private String uniqIdentify;
 	private Model model;
-	private Group group;
 	private Type type;
 	private Color color;
 	private Client client;
-	private List<Refill> reffils;
 }
