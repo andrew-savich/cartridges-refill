7b94d0999da543ea606ac4c257068fb37913c153
andrew-savich <savichandrew@Gmail.com>
2021-05-16 21:09:04 +0300

    Repository

---
+++ cartridges-refill/src/main/java/by/bajter/cartridgesrefill/repository/RefillREpository.java
@@ -0,0 +1,9 @@
+package by.bajter.cartridgesrefill.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import by.bajter.cartridgesrefill.model.Refill;
+
+public interface RefillREpository extends JpaRepository<Refill, Long>{
+	
+}
